11ad11864f0f26a1cf2d98ae7bb42e29f9cd8820
ivankozka <ikozka@thingsboard.io>
2022-04-22 21:00:24 +0300

    write comments to split work

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -43,46 +43,55 @@ public class ArrayList<E> implements List<E> {
     }
 
     @Override
+    // danya
     public E get(int idx) {
         return null;
     }
 
     @Override
+    // zadanie so *
     public boolean remove(int idx) {
         return false;
     }
 
     @Override
+    // kirill
     public boolean set(int idx, E newValue) {
         return false;
     }
 
     @Override
+    // danya
     public int size() {
         return 0;
     }
 
     @Override
+    // kirill
     public boolean removeFirst(E element) {
         return false;
     }
 
     @Override
+    // danya
     public boolean removeLast(E element) {
         return false;
     }
 
     @Override
+    // kirill
     public int indexOf(E element) {
         return 0;
     }
 
     @Override
+    // kirill
     public boolean isEmpty() {
         return false;
     }
 
     @Override
+    // danya
     public boolean contains(E value) {
         return false;
     }
