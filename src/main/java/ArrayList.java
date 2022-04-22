a944796bb0c64970266a062c737b102fc00c9af2
evtus <evtusenkodana@gmail.com>
2022-04-22 20:14:51 +0300

    add class ArrayList

---
+++ src/main/java/ArrayList.java
@@ -0,0 +1,52 @@
+public class ArrayList implements List {
+
+    @Override
+    public void add(Object element) {
+
+    }
+
+    @Override
+    public Object get(int idx) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(int idx) {
+        return false;
+    }
+
+    @Override
+    public boolean set(int idx, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean removeFirst(Object element) {
+        return false;
+    }
+
+    @Override
+    public boolean removeLast(Object element) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object element) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        return false;
+    }
+}
