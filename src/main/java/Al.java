3a60003529c34999246e40843f0571f85ffb78f0
Kirill <kolomiets.kirilo@gmail.com>
2022-04-25 14:45:44 +0200

    all done but I find out that remove() doesn't work

--- src/main/java/Al.java
+++ src/main/java/Al.java
@@ -1,6 +1,6 @@
 import java.lang.reflect.Array;
 
-public class ArrayList<E> implements List<E> {
+public class Al<E> implements List<E> {
 
     private E[] data;
     private int size;
@@ -9,7 +9,7 @@ public class ArrayList<E> implements List<E> {
     private static final double RESIZE_KOEF = 1.5;
 
     @SuppressWarnings("unchecked")
-    public ArrayList() {
+    public Al() {
         data = (E[]) Array.newInstance(Object.class, INIT_CAPACITY);
         size = 0;
     }
@@ -45,54 +45,93 @@ public class ArrayList<E> implements List<E> {
     @Override
     // danya
     public E get(int idx) {
-        return null;
+        return data[idx];
     }
 
     @Override
     // zadanie so *
     public boolean remove(int idx) {
+        if (idx < 0 || idx >= data.length) throw new ArrayIndexOutOfBoundsException();
+        try {
+                size--;
+                data[idx] = null;
+//            for (int i = idx; i < data.length; i++) {
+//                get(i);
+//                set(i, data[i + 1]);
+//                data[i + 1] = null;
+//                return true;
+//            }
+
+        } catch (Exception e) {
+        return false;
+        }
         return false;
     }
 
     @Override
     // kirill
     public boolean set(int idx, E newValue) {
-        return false;
+        if (idx < 0 || idx >= data.length) throw new ArrayIndexOutOfBoundsException();
+        data[idx] = newValue;
+        size++;
+        return true;
     }
 
     @Override
     // danya
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     // kirill
     public boolean removeFirst(E element) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == element) {
+                remove(i);
+                return true;
+            }
+        }
         return false;
     }
-
     @Override
     // danya
     public boolean removeLast(E element) {
+        for (int i = data.length - 1; i > 0; i--) {
+            if (data[i] == element) {
+                remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     // kirill
     public int indexOf(E element) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == element) {
+                return i;
+            }
+        }
         return 0;
     }
 
     @Override
     // kirill
     public boolean isEmpty() {
-        return false;
+        return size ==0;
     }
 
     @Override
     // danya
     public boolean contains(E value) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == value) {
+                return true;
+            }
+        }
         return false;
+
     }
 }
