1dec863010fd683013d10ac65b2c933879dc4324
ivankozka <ikozka@thingsboard.io>
2022-05-08 11:51:31 +0300

    add terrible exercises for my slaves

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -18,4 +18,24 @@ public interface List <E> {
     boolean isEmpty();
 
     boolean contains(E value);
+
+    boolean changeAll(Operator<E> operator);
+
+    boolean changeIf(Predicate<E> predicate, Operator<E> operator);
+
+    boolean removeIf(Predicate<E> predicate);
+
+    void forEach(Consumer<E> consumer);
+
+    boolean removeIfPresent(E elem);
+
+    boolean addAndProcess(Consumer<E> consumer);
+
+    boolean processIf(Predicate<E> predicate, Consumer<E> consumer);
+
+    <P> List<P> transform(Function<E, P> transformFunction);
+
+    <P> List<P> transform(Predicate<E> predicate, Function<E, P> transformFunction);
+
+    E reduce(BinaryOperator<E> reduceOperator);
 }
