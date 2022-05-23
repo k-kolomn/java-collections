93415131967927604514ce77b81f8595fe99d820
Kirill <kolomiets.kirilo@gmail.com>
2022-05-23 09:07:55 +0200

    add some methods to LinkedList

--- src/main/java/collections/LinkedList.java
+++ src/main/java/collections/LinkedList.java
@@ -1,29 +1,33 @@
 package collections;
 
-import function.BinaryOperator;
-import function.Consumer;
-import function.Function;
-import function.Predicate;
-import function.UnaryOperator;
+import function.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Comparator;
 import java.util.Iterator;
 
 public class LinkedList<E> implements List<E> {
 
-    @Override
-    public int size() {
-        return 0;
-    }
+
+    private Node<E> head;
+    private Node<E> tail;
+
+    private int size;
+
 
     @Override
+    public int size() {
+        return size;
+    }
+
     public Iterator<E> iterator() {
         return null;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) != -1;
     }
 
     @Override
@@ -38,16 +42,47 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public boolean add(E elem) {
-        return false;
+        var newNode = new Node<>(elem);
+
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+
+            linkNodes(head, tail);
+        } else {
+            linkNodes(tail, newNode);
+            tail = newNode;
+        }
+
+        size++;
+
+        return true;
+    }
+
+    private void linkNodes(Node<E> first, Node<E> second) {
+        first.setNext(second);
+        second.setPrevious(first);
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+            return false;
+    }
+
+    private E checkAndCastValue(Object o) {
+        if (o == null) throw new NullPointerException("Null elements is not allowed!");
+        return (E) o;
     }
 
     @Override
     public boolean removeIf(Predicate<? super E> predicate) {
+        for (int i = 0; i < size; i++) {
+            var currentNode = getNode(i);
+            if (predicate.test(currentNode.getData())) {
+                remove(currentNode.getData());
+                return true;
+            }
+        }
         return false;
     }
 
@@ -58,7 +93,9 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public void clear() {
-
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     @Override
@@ -78,12 +115,21 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public E get(int index) {
-        return null;
+        checkIndex(index);
+        if (index == 0) return head.getData();
+        if (index == size - 1) return tail.getData();
+        return getNode(index).getData();
     }
 
     @Override
-    public E set(int index, E value) {
-        return null;
+    public E set(int index, E newValue) {
+        checkIndex(index);
+
+        var currentNode = getNode(index);
+        var old = currentNode.getData();
+        currentNode.setData(newValue);
+
+        return old;
     }
 
     @Override
@@ -91,15 +137,55 @@ public class LinkedList<E> implements List<E> {
 
     }
 
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) throw new IndexOutOfBoundsException("index " + index + " is out of bounds");
+    }
+
     @Override
     public E remove(int index) {
-        return null;
+        checkIndex(index);
+
+        E old;
+
+        if (size == 1) {
+            old = head.getData();
+            clear();
+        } else if (index == 0) {
+            old = head.getData();
+            head = head.getNext();
+        } else if (index == size - 1) {
+            old = tail.getData();
+            tail = tail.getPrevious();
+        } else {
+            var currentNode = getNode(index);
+            old = currentNode.getData();
+            linkNodes(currentNode.getPrevious(), currentNode.getNext());
+        }
+
+        size--;
+
+        return old;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        var element = checkAndCastValue(o);
+
+        if (size == 0) return -1;
+
+        int counter = 0;
+        var currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.getData().equals(o)) {
+                return counter;
+            }
+
+            counter++;
+            currentNode = currentNode.getNext();
+        }
+        return -1;
     }
+
 
     @Override
     public int lastIndexOf(Object o) {
@@ -123,56 +209,180 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public boolean removeFirst(E element) {
+        if (size == 0) return false;
+
+        if (head.getData().equals(element)) {
+            remove(0);
+            return true;
+        }
+
+        for (int i = 0; i < size; i++) {
+            var currentNode = getNode(i);
+            if (currentNode.getData().equals(element)) {
+                remove(i);
+                return true;
+            }
+        }
+
         return false;
     }
 
     @Override
     public boolean removeLast(E element) {
+        if (size == 0) return false;
+
+        if (tail.getData().equals(element)) {
+            remove(size - 1);
+            return true;
+        }
+
+        for (int i = size - 1; i >= 0; i--) {
+            var currentNode = getNode(i);
+            if (currentNode.getData().equals(element)) {
+                remove(i);
+                return true;
+            }
+        }
+
         return false;
     }
 
     @Override
     public boolean changeAll(UnaryOperator<E> operator) {
-        return false;
+        for (int i = 0; i < size; i++) {
+            var currentNode = getNode(i);
+            currentNode.setData(
+                    operator.accept(
+                            currentNode.getData()
+                    )
+            );
+        }
+        size++;
+        return true;
     }
 
     @Override
     public boolean changeIf(Predicate<E> predicate, UnaryOperator<E> operator) {
-        return false;
+        for (int i = 0; i < size; i++) {
+            var currentNode = getNode(i);
+            if (predicate.test(currentNode.getData())) {
+                currentNode.setData(
+                        operator.accept(
+                                currentNode.getData()
+                        )
+                );
+            }
+        }
+        size++;
+        return true;
     }
 
     @Override
     public void forEach(Consumer<E> consumer) {
-
+        for (int i = 0; i < size; i++) {
+            consumer.accept(get(i));
+        }
     }
 
     @Override
     public boolean removeIfPresent(E elem) {
+        if (contains(elem)){
+            removeFirst(elem);
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean addAndProcess(E elem, Consumer<E> consumer) {
-        return false;
+        add(elem);
+        consumer.accept(elem);
+        return true;
     }
 
     @Override
     public boolean forEachIf(Predicate<E> predicate, Consumer<E> consumer) {
-        return false;
+        for (int i = 0; i < size; i++) {
+            var currentNode = getNode(i);
+
+            if (predicate.test(currentNode.getData())) {
+                consumer.accept(currentNode.getData());
+            }
+        }
+        return true;
     }
 
     @Override
     public <P> List<P> transform(Function<E, P> transformFunction) {
-        return null;
+        LinkedList<P> list = new LinkedList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(transformFunction.apply(get(i)));
+        }
+        return list;
     }
 
     @Override
     public <P> List<P> transform(Predicate<E> predicate, Function<E, P> transformFunction) {
-        return null;
+        LinkedList<P> list = new LinkedList<>();
+        for (int i = 0; i < size; i++) {
+            var currentNode = getNode(i);
+            if (predicate.test(currentNode.getData())) {
+                list.add(transformFunction.apply(currentNode.getData()));
+            }
+        }
+
+        return list;
     }
 
     @Override
     public E reduce(BinaryOperator<E> reduceOperator) {
-        return null;
+        if (size == 0) return null;
+        E result = head.getData();
+        for (int i = 1; i < size; i++) {
+            result = reduceOperator.apply(result, get(i));
+        }
+        return result;
+    }
+
+
+
+    private Node<E> getNode(int index) {
+        int counter;
+        if (index < size / 2) {
+            counter = 0;
+            var currentNode = head;
+            while (currentNode != null) {
+                if (index == counter) {
+                    return currentNode;
+                }
+                currentNode = currentNode.getNext();
+                counter++;
+            }
+        } else {
+            counter = size - 1;
+            var currentNode = tail;
+            while (currentNode != null) {
+                if (index == counter) {
+                    return currentNode;
+                }
+                currentNode = currentNode.getPrevious();
+                counter--;
+            }
+        }
+
+        throw new RuntimeException("Node not found!");
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class Node<T> {
+        private T data;
+
+        private Node<T> next;
+        private Node<T> previous;
+
+        public Node(T data) {
+            this.data = data;
+        }
     }
 }
